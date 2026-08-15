ffca601c4ce848e9cd8406d5a507594c32a443ba
Raul M. <67912886+DataRaul@users.noreply.github.com>
2026-08-15 11:51:15 +0200

    Expose application version

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+export const APP_VERSION = "3.7.0";
 export const DATASET_URL = "https://raw.githubusercontent.com/hasaneyldrm/exercises-dataset/main/data/exercises.json";
 export const MEDIA_BASE_URL = "https://raw.githubusercontent.com/hasaneyldrm/exercises-dataset/main/";
 export const DATASET_REPOSITORY = "https://github.com/hasaneyldrm/exercises-dataset";
